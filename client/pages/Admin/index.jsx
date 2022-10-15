af9522e0fc747203f56abf8dc22d6d0ebc71f878
tonmoy5 <tonmoy52532@gmail.com>
2022-10-15 14:54:11 +0600

    some changes

---
+++ client/pages/Admin/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../../components/Navbar"
+
+const index = () => {
+  return (
+    <div className="min-h-screen min-w-screen gradient-bg-services">
+      <Navbar />
+    </div>
+  )
+}
+
+export default index
